c94d5a10840286e2c42e131c18fd1bed5a27b425
Annaprusakova <hanna.prusakova@gmail.com>
2023-02-19 19:06:10 +0200

    add styles variables

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,5 +1,16 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Review from "./Review";
+import Solution from "./Solution";
+
 const MainPage: React.FC<{}> = () => {
-    return (<div></div>);
+    return (
+    <div>
+        <Header />
+        <Solution />
+        <Review />
+        <Footer />
+    </div>);
 }
 
 export default MainPage;
---
+++ src/components/Solution.tsx
@@ -0,0 +1,5 @@
+const Solution: React.FC<{}> = () => {
+    return (<div></div>);
+}
+
+export default Solution;
